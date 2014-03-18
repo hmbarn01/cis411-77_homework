c8d5d2b5db610b12777c2175949a844022b6f1be
hmbarn01 <hmbarn01@louisville.edu>
2014-03-17 21:06:05 -0400

    Pet Store - Views Added

---
+++ homework6/Homework6/Controllers/InventoryController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PetStore.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
