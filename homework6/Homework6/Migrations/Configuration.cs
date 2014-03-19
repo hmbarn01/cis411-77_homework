fb3ccc4114a3395f09a170fa5d0aebc605d75b37
hmbarn01 <hmbarn01@louisville.edu>
2014-03-19 00:05:34 -0400

    Before "Initial" Migration

--- homework6/Homework6/Migrations/Configuration.cs
+++ homework6/Homework6/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Homework6.Migrations
 {
+    using Homework6.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +15,44 @@ namespace Homework6.Migrations
 
         protected override void Seed(Homework6.Models.PetDBContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Pets.AddOrUpdate(i => i.Name,
+                new Pet
+                {
+                      ID = 1,
+                      Name = "Woofy",
+                      DescriptionShort = "Woofy is cute.",
+                      DescriptionLong = "Woofy is reeeeally reallllllyyyyy cute.",
+                      DateReceived = DateTime.Parse("1989-1-11"),
+                      Quantity = 1,
+                      Price = 100m
+                },
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                new Pet
+                {
+                      ID = 2,
+                      Name = "Doggeh",
+                      DescriptionShort = "Doggeh is cute.",
+                      DescriptionLong = "Doggeh is reeeeally reallllllyyyyy cute.",
+                      DateReceived = DateTime.Parse("1989-2-11"),
+                      Quantity = 1,
+                      Price = 100m
+                },
+
+                new Pet
+                {
+                      ID = 3,
+                      Name = "Urf",
+                      DescriptionShort = "Urf is cute.",
+                      DescriptionLong = "WoUrfofy is reeeeally reallllllyyyyy cute.",
+                      DateReceived = DateTime.Parse("1989-3-11"),
+                      Quantity = 1,
+                      Price = 100m
+                }
+
+
+                 
+           );
+
         }
     }
 }
