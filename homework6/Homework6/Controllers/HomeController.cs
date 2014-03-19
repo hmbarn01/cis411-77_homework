01af17287f275b1320c11297dfe15ef476659f56
hmbarn01 <hmbarn01@louisville.edu>
2014-03-18 20:55:36 -0400

    Changed Web.config
    Added connection string to Pet Database

--- homework6/Homework6/Controllers/HomeController.cs
+++ homework6/Homework6/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace Homework6.Controllers
 {
     public class HomeController : Controller
     {
-     {
         public ActionResult Index()
         {
             return View();
---
+++ homework6/Homework6/Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Homework6.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DescriptionShort{ get; set; }
+        public string DescriptionLong { get; set; }
+        public DateTime DateReceived  { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
