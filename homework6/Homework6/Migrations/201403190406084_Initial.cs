bc20936ee08b0fb0115d8f3f631c96727b12b91b
hmbarn01 <hmbarn01@louisville.edu>
2014-03-19 01:06:23 -0400

    Pre-first deploy

---
+++ homework6/Homework6/Migrations/201403190406084_Initial.cs
@@ -0,0 +1,31 @@
+namespace Homework6.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Pets",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        DescriptionShort = c.String(),
+                        DescriptionLong = c.String(),
+                        DateReceived = c.DateTime(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Pets");
+        }
+    }
+}
