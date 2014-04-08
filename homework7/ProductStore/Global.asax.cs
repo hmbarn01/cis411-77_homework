7c62c00feabc3a1a10248e59aa0ac81d9033c050
hmbarn01 <hmbarn01@louisville.edu>
2014-04-07 23:16:47 -0400

    Page 3 complete

--- homework7/ProductStore/Global.asax.cs
+++ homework7/ProductStore/Global.asax.cs
@@ -23,6 +23,7 @@ namespace ProductStore
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+            System.Data.Entity.Database.SetInitializer(new ProductStore.Models.OrdersContextInitializer());
         }
     }
 }
