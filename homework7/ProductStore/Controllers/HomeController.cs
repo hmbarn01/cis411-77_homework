b57e5a853c29a8e391318fb6a36406ab21ed9b6f
hmbarn01 <hmbarn01@louisville.edu>
2014-04-07 23:49:15 -0400

    Page 4 complete

--- homework7/ProductStore/Controllers/HomeController.cs
+++ homework7/ProductStore/Controllers/HomeController.cs
@@ -29,11 +29,9 @@ namespace ProductStore.Controllers
             return View();
         }
 
+        [Authorize(Roles = "Administrator")]
         public ActionResult Admin()
         {
-            string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "admin", });
-            ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
-
             return View();
         }
     }
